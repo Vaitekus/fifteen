166ab951089aa4b2fa4b79682fe3bf911d9b1ef4
olga.vaitekus <olga.vaitekus@p2h.com>
2019-05-12 21:54:58 +0300

    fix comments

--- maze/main.js
+++ maze/main.js
@@ -1,14 +1,18 @@
 (function(){
-  const GAME_SETTINGS = {
-    color: [-1, 0, 1, 2, 3], //black, white, green, pink, red
-    colorName: ['black', 'white', 'green', 'pink', 'red'],
-  };
+  const CELL_COLORS = {
+    start: 'green', 
+    finish: 'pink',
+    path: 'red',
+    wall: 'black',
+    ground: 'white'
+  }
   const holder = document.getElementById("table");
   const cellArray = [];
   const startX = 0;
   const startY = 0;
   const endX = 7;
   const endY = 6;
+  const percentChance = 0.6;
   let grid = [
       [0, -1, 0, -1, -1, -1, 0, 0, 0, 0],
       [0, -1, 0, 0, 0, 0, 0, -1, 0, -1],
@@ -43,61 +47,44 @@
     return JSON.parse(JSON.stringify(array));
   }
 
-  function generateRandomMaze(rows, cells, x1, y1, x2, y2) {
+  function generateRandomMaze(rows, cells, startX, startY, endX, endY) {
     let generatedGrid;
     let cloneGrid;
 
     do {
-      generatedGrid = [];
-      cloneGrid = [];
-      generatedGrid = generateGrid(rows, cells, x1, y1, x2, y2);
+      generatedGrid = generateGrid(rows, cells, startX, startY, endX, endY);
       cloneGrid = cloneArray(generatedGrid);
-    } while (findSearch(cloneGrid, x1, y1, x2, y2)[1] === 0);
+    } while (changeMatrix(cloneGrid, startX, startY)[endX][endY] === 0);
 
     return generatedGrid;
   }
 
-  function generateGrid(rows, cells, x1, y1, x2, y2) {
-    const percentChance = 0.6;
-    let newGeneratedGrid = [];
+  function generateGrid(rows, cells, startX, startY, endX, endY) {
+    let GeneratedMatrix = [];
 
     for (let i = 0; i < rows; i++){
-      newGeneratedGrid.push([]);
+      GeneratedMatrix.push([]);
 
       for (let j=0; j < cells; j++) {
-        if (Math.random() < percentChance) {
-          newGeneratedGrid[i].push(0);
-        } else {
-          newGeneratedGrid[i].push(-1);
-        }
+        GeneratedMatrix[i].push(Math.random() < percentChance ? 0 : -1)
       }
     }
-    newGeneratedGrid[x1][y1] = 0;
-    newGeneratedGrid[x2][y2] = 0;
+    GeneratedMatrix[startX][startY] = 0;
+    GeneratedMatrix[endX][endY] = 0;
 
-    return newGeneratedGrid;
+    return GeneratedMatrix;
   }
 
   function createMaze() {
-    let cell;
-
+  
     for(let i = 0; i < gridHeight; i++) {
       const currentRow = document.createElement('tr');
       holder.appendChild(currentRow);
 
       for(let j = 0; j < gridWidth; j++) {
-        if(i == startX && j == startY) {
-          cell = addBackground(1);
-        } else if (i == endX && j == endY) {
-          cell = addBackground(2);
-        } else {
-          cell = addBackground(grid[i][j]);
-        }
-
-        cell.setAttribute('class', 'cell');
+        const cell = createCell(i, j);
+        
         currentRow.appendChild(cell);
-        cell.xCoordinate = i;
-        cell.yCoordinate = j;
         cellArray.push(cell);
       }
     }
@@ -106,31 +93,29 @@
     grid[endX][endY] = 0;
   };
 
-  function addBackground(value) {
-    const currentCell = document.createElement('td');
-    
-    switch (value) {
-      case -1:
-      currentCell.style.background = GAME_SETTINGS.colorName[0];
-        break;
-      case 0:
-      currentCell.style.background = GAME_SETTINGS.colorName[1];
-        break;
-      case 1:
-      currentCell.style.background = GAME_SETTINGS.colorName[2];
-        break;
-      case 2:
-      currentCell.style.background = GAME_SETTINGS.colorName[3];
-        break;
-      default:
-      currentCell.style.background = GAME_SETTINGS.colorName[4];
+  function createCell(cellY, cellX) {
+    const cell = document.createElement('td');
+
+    if (cellY == startX && cellX == startY) {
+      cellBackground = CELL_COLORS.start;
+    } else if (cellY == endX && cellX == endY) {
+      cellBackground = CELL_COLORS.finish;
+    } else if (grid[cellY][cellX] == -1) {
+      cellBackground = CELL_COLORS.wall;
+    } else {
+      cellBackground = CELL_COLORS.ground;
     }
+    
+    cell.setAttribute('class', 'cell');
+    cell.style.background = cellBackground;
+    cell.xCoordinate = cellY;
+    cell.yCoordinate = cellX;
 
-    return currentCell;
-  };
+    return cell;
+  }
 
-  function findSearch(matrix, x1, y1, x2, y2) {
-    let toVisitGrid = [[x1, y1]];
+  function changeMatrix(matrix, startX, startY) {
+    let toVisitGrid = [[startX, startY]];
 
     while(toVisitGrid.length) {
       let xPosition = toVisitGrid[0][0];
@@ -138,7 +123,7 @@
      
       for (var i = xPosition-1; i < xPosition+2; i++) {
         for (var j = yPosition-1; j < yPosition+2; j++) {
-          if (isValid(matrix, i, j, x1, y1, 0, xPosition, yPosition)) {
+          if (isValid(matrix, i, j, startX, startY, 0, xPosition, yPosition)) {
             matrix[i][j] = matrix[xPosition][yPosition] + 1;
             toVisitGrid.push([i, j]);
           }
@@ -146,53 +131,52 @@
       }
       toVisitGrid.shift(); 
     }
-    
-    const distance = matrix[x2][y2];
-    return [matrix, distance];
+    return matrix;
   }; 
 
-   function isValid (matrix, i, j, x1, y1, value, x, y) {
-     return matrix[i] && (matrix[i][j] === value) && !(i === x && j === y) && !(i === x1 && j === y1) && (x === i || y === j);
+   function isValid (matrix, i, j, startX, startY, value, x, y) {
+     return matrix[i] && (matrix[i][j] === value) && !(i === x && j === y) && !(i === startX && j === startY) && (x === i || y === j);
   };
 
-  function backtrace(matrix, x1, y1, x2, y2) { 
-    let previousValue = matrix[x2][y2];
+  function backtrace(matrix, startX, startY, endX, endY) { 
+    let previousValue = matrix[endX][endY];
     const successfullRoute = [];
-    let x = x2;
-    let y = y2;
+    let x = endX;
+    let y = endY;
     
-    while (!(x === x1 && y === y1) ) {
+    while (!(x === startX && y === startY) ) {
       for (var i = x-1; i < x+2; i++) {
         for (var j = y-1; j < y+2; j++){
-          if (
-            matrix[i] && (matrix[i][j] === previousValue -1) && !(i === x && j === y) ) {
+          if (matrix[i] && (matrix[i][j] === previousValue -1) && !(i === x && j === y) ) {
             previousValue = matrix[i][j];
             successfullRoute.push([i, j]);
             x = i;
             y = j;
-          } else if (successfullRoute.length == matrix[x2][y2] - 1) {
-            x = x1;
-            y = y1;
+          } else if (successfullRoute.length == matrix[endX][endY] - 1) {
+            x = startX;
+            y = startY;
+          } else {
+            continue;
           }
         }
       }   
     }
 
-    successfullRoute.unshift([x2, y2]);
-    successfullRoute.push([x1, y1]);
+    successfullRoute.unshift([endX, endY]);
+    successfullRoute.push([startX, startY]);
     return successfullRoute.reverse();
 };
 
-function drowPath(matrix, x1, y1, x2, y2) {
-  let newGrid = findSearch(matrix, x1, y1, x2, y2);
-  const route = backtrace(newGrid[0], x1, y1, x2, y2);
+function drowPath(matrix, startX, startY, endX, endY) {
+  let createdGrid = changeMatrix(matrix, startX, startY);
+  const route = backtrace(createdGrid, startX, startY, endX, endY);
 
   for (var i = 0; i < route.length; i++) {
     let x = route[i][0];
     let y = route[i][1];
 
     cellArray.forEach(cell => {
-      if(cell.xCoordinate === x && cell.yCoordinate === y) cell.style.background = GAME_SETTINGS.colorName[4];;
+      if(cell.xCoordinate === x && cell.yCoordinate === y) cell.style.background = CELL_COLORS.path;
     })
   }
 }
